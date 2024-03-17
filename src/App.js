99f578c0c4e25d5984681a7bb6a2b55e8c214301
Md. Asif Imtiyaj Chowdhury <asifimtiyajchowdhury@gmail.com>
2024-03-18 03:30:39 +0600

    chat room in chatengine

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
 import { ChatEngine } from "react-chat-engine";
 
 import './App.css';
+
+//app component will be a simple app component
+//and it'll have just one return statement. Calling the chat engine which is basically a component.
+const App = () => { 
+    return (
+        <ChatEngine 
+            height = "100vh"
+            projectID = "7115d3c2-6564-4cc5-9ac7-6f4095c5ed0b"
+            userName = "asifimtiyajchowdhury"
+            userSecrect = "asifniloy12345"
+        />
+    )
+}
+
+export default App;
