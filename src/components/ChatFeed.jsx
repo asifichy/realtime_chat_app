3dce03e09a8954d8a587db0a9c6e6af425329aea
Md. Asif Imtiyaj Chowdhury <asifimtiyajchowdhury@gmail.com>
2024-03-26 02:17:45 +0600

    major bug fixed in mentioning the 'MyMessage' component

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -5,68 +5,6 @@ import MyMessage from './MessageForm';
 import TheirMessage from './TheirMessage';
 import Messageform from './MessageForm';
 
-// const ChatFeed = (props) => {
-//     const { chats, activeChat, userName, messages } = props;
-
-//     //finding current chats
-//     const chat = chats && chats[activeChat];
-
-//     // functional component for generating messages
-//     const renderMessages = () => {
-//         const keys = Object.keys(messages);
-
-//         // the keys are basically id's of messages
-
-//         return keys.map((key, index) => {
-//             const message = messages[keys];
-//             const lastMessageKey = index === 0 ? null : keys[index-1]; //ensuring if there are messages then find the last message
-//             const MyMessage = userName === message.sender.userName;
-
-//             return(
-//                 <div key={ `msg_${index}`} style={{ width: '100%' }}>
-//                     <div className="message-block">
-//                         {
-//                             // isMyMessage
-//                             // ? <MyMessage message = {message} /> 
-//                             // : <TheirMessage message = {message} lastMessage = {messages[lastMessageKey]} />
-//                         }
-//                     </div>
-//                     <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px': '68px' }}>
-//                         read-receipts
-//                     </div>
-//                 </div>
-//             )
-//         });
-//     }
-
-//     //loading effect
-//     if(!chat){
-//         return 'Loading ...';
-//     }
-
-//     return ( //structure of our chat feed
-//         <div className="chat-feed">
-//             <div className="chat-title-container">
-//                 <div className="chat-title">
-//                     {
-//                         chat?.title
-//                     }
-//                 </div>
-//                 <div className="chat-subtitle">
-//                     {
-//                         chat.people.map((person) => ` ${person.person.userName}`)
-//                     }
-//                 </div>
-//             </div>
-//             {renderMessages()}
-//             <div style={{ height: '100px' }}>
-//                     <div className="message-form-container">
-//                         <Messageform {...props} chatId = {activeChat} />
-//                     </div>
-//             </div>
-//         </div>
-//     );
-// }
 
 const ChatFeed = (props) => {
     const { chats, activeChat, userName, messages } = props;
@@ -81,15 +19,51 @@ const ChatFeed = (props) => {
         return keys.map((key, index) => {
             const message = messages[key];
             const lastMessageKey = index === 0 ? null : keys[index-1]; //ensuring if there are messages then find the last message
-            const MyMessage = userName === message.sender.userName;
+            const IsMyMessage = userName === message.sender.userName;
+
+
+            return (
+                <div key={`msg_&{index}`} style={{ width: '100%' }}>
+                    <div className="message-block">
+                        {
+                            IsMyMessage 
+                            ? <MyMessage message = {message} />
+                            : <TheirMessage message = {message} lastMessage = {messages[lastMessageKey]} />
+                        }
+                    </div>
+                    <div className="read-receipts" style={{ marginRight: IsMyMessage ? '18px' : '0px', marginLeft: IsMyMessage ? '0px' : '68px' }}>
+
+                    </div>
+                </div>
+            )
         })
     }
 
+    //loading effect
+    if(!chat){
+        return 'Loading ...';
+    }
 
-
-    return (
-        <div>
-            ChatFeed
+    return ( //structure of our chat feed
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">
+                    {
+                        chat?.title
+                    }
+                </div>
+                <div className="chat-subtitle">
+                    {
+                        chat.people.map((person) => ` ${person.person.userName}`)
+                    }
+                </div>
+            </div>
+            {renderMessages()}
+            <div style={{ height: '100px' }}>
+                    <div className="message-form-container">
+                        <Messageform {...props} chatId = {activeChat} />
+                    </div>
+            </div>
         </div>
     );
 }
